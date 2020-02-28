28bd8e9deb9055eb0c0cf157bd154a02439f8b05
RaytheMage <ray@Rachels-MacBook-Pro.local>
2020-02-27 20:07:45 -0700

    practice with film js moved onto people

--- Starwarssection/starwars.js
+++ Starwarssection/starwars.js
@@ -1,27 +1,4 @@
-console.log('Hi There! Welcome to my page')
-// import { films } from './films.js'
+import { films } from './Data/films.js'
+import { films } from './Data/people.js'
 
-// let filmSort = films.sort((a,b) => (a.episode_id > b.episode_id) ? 1 : -1)
-
-// const movie = document.querySelector('.movie')
-
-// films.forEach((filmSort) => {
-
-// let tile = document.createElement('div')
-
-// let titleElement = document.createElement("h1")
-// let crawlElement = document.createElement("p")
-
-// titleElement.textContent = filmSort.title
-// crawlElement.textContent = filmSort.opening_crawl
-
-// tile.appendChild(titleElement)
-// tile.appendChild(crawlElement)
-// movie.appendChild(tile)
-// })
-
-// let titleElement = document.querySelector('.title')
-// let crawlElement = document.querySelector('.crawl')
-
-// titleElement.textContent = films[0].title
-// crawlElement.textContent = films[0].opening_crawl
+const greetingDiv = document.querySelector('.greeting')
