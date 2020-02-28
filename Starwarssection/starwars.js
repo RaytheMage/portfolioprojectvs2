48c9f4bbc74e150bde8ea5f623aef2f8d9fd7dfe
RaytheMage <ray@Rachels-MacBook-Pro.local>
2020-02-27 21:09:41 -0700

    worked with people.js followed lesson 02-25-2020

--- Starwarssection/starwars.js
+++ Starwarssection/starwars.js
@@ -1,4 +1,33 @@
 import { films } from './Data/films.js'
-import { films } from './Data/people.js'
+import { people } from './Data/people.js'
+import { planets } from './Data/planets.js'
+import { species } from './Data/species.js'
+import { starships } from './Data/starships.js'
+import { vehicles } from './Data/vehicles.js'
 
 const greetingDiv = document.querySelector('.greeting')
+
+const castList = document.createElement('ul')
+
+let counter = 1
+
+people.forEach(person => {
+    let listItem = document.createElement('li')
+    listItem.textContent = person.name
+    castList.appendChild(listItem)
+
+    let anchorWrap = document.createElement('a')
+    anchorWrap.href = '#'
+
+    let imageItem = document.createElement('img')
+    imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
+    // add some way to handle user clicks on the image
+    imageItem.addEventListener('click', (event) => {
+        console.log(event)
+    })
+    anchorWrap.appendChild(imageItem)
+    greetingDiv.appendChild(anchorWrap)
+    counter ++
+})
+
+greetingDiv.appendChild(castList)
