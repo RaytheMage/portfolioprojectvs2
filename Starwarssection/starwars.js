264f9d27b76832706adcc23117ccf27c16d586fa
RaytheMage <ray@Rachels-MacBook-Pro.local>
2020-02-27 22:30:18 -0700

    added buttons and gender roles

--- Starwarssection/starwars.js
+++ Starwarssection/starwars.js
@@ -6,6 +6,22 @@ import { starships } from './Data/starships.js'
 import { vehicles } from './Data/vehicles.js'
 
 const greetingDiv = document.querySelector('.greeting')
+const maleButton = document.querySelector('#male')
+const femaleButton = document.querySelector('#female')
+const otherButton = document.querySelector('#other')
+
+const maleCharacters = people.filter(person => person.gender === 'male')
+console.log(maleCharacters)
+
+const femaleCharacters = people.filter(person => person.gender === 'female')
+console.log(femaleCharacters)
+
+const otherCharacters = people.filter(person => {
+    if (person.gender === 'hermaphrodite' || person.gender === 'n/a') {
+    return person}
+})
+
+console.log(otherCharacters)
 
 let counter = 1
 
@@ -31,4 +47,8 @@ people.forEach(person => {
     anchorWrap.appendChild(imageItem)
     greetingDiv.appendChild(anchorWrap)
     counter ++
+})
+
+maleButton.addEventListener('click', (event) => {
+    console.log('clicked on male button')
 })
