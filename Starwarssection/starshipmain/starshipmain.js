86dfbd266e2b10c9a900e448aa40ce5dbe8f3112
RaytheMage <rachel.rasmussen.511@gmail.com>
2020-03-11 22:23:55 -0600

    Class 3-5-20 Created list for starships

--- Starwarssection/starshipmain/starshipmain.js
+++ Starwarssection/starshipmain/starshipmain.js
@@ -1,3 +1,25 @@
 import { starships } from '../Data/starships.js'
 
 const nav = document.querySelector('.nav')
+
+const navList = document.querySelector('.navlist')
+
+function populateNav(starships) {
+    starships.forEach(starship => {
+
+        let anchorWrap = document.createElement('a')
+        anchorWrap.href = '#'
+        anchorWrap.addEventListener('click', event => {
+         console.log(event)
+        })
+
+        let listItem = document.createElement('li')
+        listItem.textContent = starship.name
+
+        anchorWrap.appendChild(listItem)
+        navList.appendChild(anchorWrap)
+        // nav.appendChild(navList)
+    })
+}
+
+populateNav(starships)
