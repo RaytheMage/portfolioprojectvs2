ecb8a8c6128b36efc318781f81ab3c0fcb3ef684
RaytheMage <rachel.rasmussen.511@gmail.com>
2020-04-01 01:43:19 -0600

    fixed javascript to class 3-26

--- congressproject/senators/ussenate.js
+++ congressproject/senators/ussenate.js
@@ -1,16 +1,44 @@
-import {senators} from '../Data/senators.js'
+import { senators } from '../data/senators.js'
 
-const senatorDiv = document.createElement('div')
-const main = document.querySelector('main')
+const senatorDiv = document.querySelector('.senators')
 
 function populateSenatorDiv() {
     senators.forEach(senator => {
-        let senatorName = `${senator.first_name} ${senator.last_name}`
-        let nameParagraph = document.createElement('p')
-        nameParagraph.textContent = senatorName
-        senatorDiv.appendChild(nameParagraph)
+        let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+
+        let senFigure = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document.createElement('figcaption')
+            
+        figImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
+        figCaption.textContent = `${senator.first_name}${middleName}${senator.last_name}`
+
+        senFigure.appendChild(figImg)
+        senFigure.appendChild(figCaption)
+        senatorDiv.appendChild(senFigure)
     })
-    main.appendChild(senatorDiv)
+    // main.appendChild(senatorDiv)
 }
+
+const filterSenators = (prop, value) => {
+    return senators.filter(senator => {
+        return senator[prop] === value
+    })
+}
+
+/* console.log(filterSenators('party', 'R'))
+console.log(filterSenators('party', 'D'))
+console.log(filterSenators('party', 'ID')) */
+
+const senatorNames = senators.map(senator => {
+    let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+    return {
+        id: senator.id,
+        name: `${senator.first_name}${middleName}${senator.last_name}`,
+        imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
+    }
+})
+
+//https://www.govtrack.us/static/legislator-photos/300002-200px.jpeg
 
 populateSenatorDiv()
