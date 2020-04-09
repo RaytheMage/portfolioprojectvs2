25612f6858ad230cd2a9c8bd8d7f2a9906cfb0a5
RaytheMage <rachel.rasmussen.511@gmail.com>
2020-04-09 11:58:50 -0600

    finished some stuff on the senate started pokemon

--- congressproject/senators/ussenate.js
+++ congressproject/senators/ussenate.js
@@ -9,7 +9,10 @@ function getSimplifiedSenators(senatorArray){
         id: senator.id,
         name: `${senator.first_name}${middleName}${senator.last_name}`,
         imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
-        seniority: parseInt(senator.seniority, 10)
+        seniority: parseInt(senator.seniority, 10),
+        missedVotesPct: senator.missed_votes_pct,
+        party: senator.party,
+        loyaltyPct: senator.votes_with_party_pct
     }
 })
 }
@@ -37,13 +40,21 @@ const filterSenators = (prop, value) => {
 }
 
 const republicans = filterSenators('party', 'R')
+const democrats = filterSenators('party', 'D')
 
-const mostSeniority = getSimplifiedSenators(republicans).reduce(
-        (acc, senator) => {
-        return acc.seniority > senator.seniority ? acc : senator
-    }
-)
+const mostSeniority = getSimplifiedSenators(senators).reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
 
-console.log(mostSeniority)
+const missedVotes = getSimplifiedSenators(senators).reduce((acc, senator) => acc.missedVotesPct > senator.missedVotesPct ? acc : senator)
 
-populateSenatorDiv(getSimplifiedSenators(republicans))
+let loyalArray = []
+
+const mostLoyal = getSimplifiedSenators(republicans).reduce((acc, senator) => {
+        if (senator.loyaltyPct === 100) {
+            loyalArray.push(senator)
+        }
+            return acc.loyaltyPct > senator.loyaltyPct ? acc : senator
+    })
+
+console.log(loyalArray)
+
+populateSenatorDiv(getSimplifiedSenators(senators))
