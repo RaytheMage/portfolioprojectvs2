0c319bd3e3d8737730cd17af3296d6c877bbb152
RaytheMage <rachel.rasmussen.511@gmail.com>
2020-04-19 02:38:19 -0600

    senate not working but wrote down code from 4-16

--- congressproject/senators/ussenate.js
+++ congressproject/senators/ussenate.js
@@ -20,17 +20,58 @@ function getSimplifiedSenators(senatorArray){
 function populateSenatorDiv(simpleSenators) {
     console.log(simpleSenators)
     simpleSenators.forEach(senator => {
+        let senDiv = document.createElement('div') 
         let senFigure = document.createElement('figure')
         let figImg = document.createElement('img')
         let figCaption = document.createElement('figcaption')
-            
+        let partyIcon = document.createElement('i')  
+        if (senator.party === 'R') partyIcon.className = 'fas fa-republican'
+        if (senator.party === 'D') partyIcon.className = 'fas fa-democrat'
+        if (senator.party === 'ID') partyIcon.className = 'fas fa-star'
         figImg.src = senator.imgURL
         figCaption.textContent = senator.name
 
+        figCaption.appendChild(partyIcon)
         senFigure.appendChild(figImg)
         senFigure.appendChild(figCaption)
-        senatorDiv.appendChild(senFigure)
+        senDiv.appendChild(senFigure)
+        senDiv.appendChild(progressBars(senator))
+        senatorGrid.appendChild(sendiv)
     })
+}
+
+function progressBars(senator) {
+    let progressDiv = document.createElement('div')
+    progressDiv.className = 'progressDiv'
+    let loyaltyLabel = document.createElement('label')
+    loyaltyLabel.for = 'loyalty'
+    loyaltyLabel.textContent = 'Loyalty'
+    let loyaltyBar = document.createElement('progress')
+    loyaltyBar.id = 'loyalty'
+    loyaltyBar.max = 100
+    loyaltyBar.value = senator.loyaltyPct
+    let seniorityLabel = document.createElement('label')
+    seniorityLabel.for = 'seniority'
+    seniorityLabel.textContent = 'Seniority'
+    let seniorityBar = document.createElement('progress')
+    seniorityBar.id = 'seniority'
+    seniorityBar.max = 100
+    seniorityBar.value = parseInt((senator.seniority / mostSeniority) * 100)
+    let votingLabel = document.createElement('label')
+    votingLabel.for = 'voting'
+    votingLabel.textContent = "Vote"
+    let votingBar = document.createElement('label')
+    votingLabel.id = 'voting'
+    votingBar.max = 100
+    votingBar.value = 10
+
+    progressDiv.appendChild(loyaltyLabel)
+    progressDiv.appendChild(loyaltyBar)
+    progressDiv.appendChild(seniorityLabel)
+    progressDiv.appendChild(seniorityBar)
+    progressDiv.appendChild(votingLabel)
+    progressDiv.appendChild(votingBar)
+    return progressDiv
 }
 
 const filterSenators = (prop, value) => {
@@ -55,6 +96,7 @@ const mostLoyal = getSimplifiedSenators(republicans).reduce((acc, senator) => {
             return acc.loyaltyPct > senator.loyaltyPct ? acc : senator
     })
 
-console.log(loyalArray)
+// console.log(loyalArray)
 
 populateSenatorDiv(getSimplifiedSenators(senators))
+console.log(mostSeniority.seniority)
