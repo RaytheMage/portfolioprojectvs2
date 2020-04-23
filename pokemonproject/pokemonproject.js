c75fb431d5a1e78b78648097f67b1c697d8f3b5b
RaytheMage <rachel.rasmussen.511@gmail.com>
2020-04-22 19:17:21 -0600

    more stats on poke cards filled out

--- pokemonproject/pokemonproject.js
+++ pokemonproject/pokemonproject.js
@@ -56,7 +56,7 @@ function populateCardFront(pokemon) {
     let frontImage = document.createElement('img')
     frontImage.src = `/pokemonproject/pokeimages/${getImageFileName(pokemon)}.png`
 
-    let frontLabel = document.createElement('p')
+    let frontLabel = document.createElement('h1')
     frontLabel.textContent = `${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}`
     cardFront.appendChild(frontImage)
     cardFront.appendChild(frontLabel)
@@ -76,12 +76,33 @@ function getImageFileName(pokemon) {
 function populateCardBack(pokemon) {
     let cardBack = document.createElement('div')
     cardBack.className = 'card__face card__face--back'
+
+    let backName = document.createElement('h1')
+    backName.textContent = `${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}`
+
+    let info = document.createElement('h2')
+    info.textContent = 'Information'
+
+    let height = document.createElement('p')
+    height.textContent = 'Height' + ': ' + `${pokemon.height.toFixed([1])}` / 10 + 'm' 
+
+    let weight = document.createElement('p')
+    weight.textContent = 'Weight' + ': ' + `${pokemon.weight.toFixed([1])}` /10 + 'kg'
+
+    let abilities = document.createElement('h2')
+    abilities.textContent = 'Abilities'
+
     let abilityList = document.createElement('ul')
     pokemon.abilities.forEach(ability => {
         let abilityName = document.createElement('li')
         abilityName.textContent = ability.ability.name
         abilityList.appendChild(abilityName)
     })
+    cardBack.appendChild(backName)
+    cardBack.appendChild(info)
+    cardBack.appendChild(height)
+    cardBack.appendChild(weight)
+    cardBack.appendChild(abilities)
     cardBack.appendChild(abilityList)
     return cardBack
 }
